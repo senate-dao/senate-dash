f6737a388c36cbe750a0c8cd6bc4ac6d64f1f073
nebularthm <michael@tryfabric.com>
2022-07-24 06:42:14 -0400

    added in tabs and loading tot he forums page

--- pages/forms/index.tsx
+++ pages/forms/index.tsx
@@ -1,12 +1,28 @@
 import * as React from "react";
 import ForumList from "./components/ForumList";
+import { Tab, Tabs, Box } from "@mui/material";
 interface VoterProps {
   onSubmit: () => void;
 }
 function Forms(props: VoterProps) {
+  const [dao, setDao] = React.useState("Balancer");
+  const handleChange = (event, newValue) => {
+    setDao(newValue);
+  };
   return (
     <div>
-      <ForumList></ForumList>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={dao}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Balancer" value="Balancer" />
+          <Tab label="Jet" value="Jet" />
+          <Tab label="Optimism" value="Optimism" />
+        </Tabs>
+      </Box>
+      <ForumList key={dao} dao={dao}></ForumList>
     </div>
   );
 }
